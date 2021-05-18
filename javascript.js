eed87b62d112e00d20579ca4fedcb5eeb1c002a3
Masco <phalsco@web.de>
2021-05-18 10:55:28 +0200

    Done

--- javascript.js
+++ javascript.js
@@ -7,16 +7,14 @@
 //     console.log(document.getElementById('question').classList.value);
 // }
 
-function toggle(num) {
-    var object = document.getElementById('q'+num).classList;
-    var classArray = document.getElementsByClassName('question');
-    for (var i = 0; i < classArray.length; i++) {
-        if (classArray[i].classList.contains('show')) {
-            object.replace('show', 'hide');
-        }
-    }
-    object.replace('hide', 'show');
-}
+// function toggle(num) {
+//     var object = document.getElementById('q'+num).classList;
+//     var objectShow = document.getElementsByClassName('show').classList;
+//     for (var i = 0; i < objectShow.length; i++) {
+//         objectShow.replace('show', 'hide');
+//     }
+//     object.replace('hide', 'show');
+// }
 
 // function toggle(num) {
 //     var currentObject = document.getElementById('q'+num).classList;
@@ -26,3 +24,20 @@ function toggle(num) {
 //         currentObject.replace('show','hide');
 //     }
 // }
+
+function toggle(num) {
+    var idArray = new Array('q1','q2','q3','q4','q5');
+    for (var i = 0; i < idArray.length; i++) {
+        document.getElementById(idArray[i]).classList.replace('show','hide');
+    }
+    if (document.getElementById('q'+num).classList.contains('hide')) {
+        document.getElementById('q'+num).classList.replace('hide','show');
+    } else {
+        document.getElementById('q'+num).classList.replace('show','hide');
+    }
+}
+
+function mouse(num) {
+    var object = document.getElementById('q'+num);
+    object.classList.toggle('yes');
+}
